f8cec37fd90351440094701920e76ec4a34f5677
Fran <fmjerez@icloud.com>
2020-07-09 12:24:00 +0200

    Componente_2

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+//imr = import React from 'react';
+
+// sfc  = const  = () => {
+//     return (  );
+// }
+
+// export default ;
+
+// const Footer = () => {
+//   return (
+//     <footer>
+//       <p>Todos los derechos reservados &copy; </p>
+//     </footer>
+//   );
+// };
+
+//En function expresion, si no tenemos código js, sino que solo es un return podemos hacerlo de la siguiente manera:
+const Footer = () => (
+  <footer>
+    <p>Todos los derechos reservados &copy; </p>
+  </footer>
+);
+
+export default Footer;
