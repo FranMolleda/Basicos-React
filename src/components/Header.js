2e5c6305b7a1863210462b3c62fb002f5fcf1a7b
Fran <fmjerez@icloud.com>
2020-07-09 11:36:02 +0200

    Básicos JSX

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Header() {
-  return <h1> Desde el Header </h1>;
+  const edad = 16;
+
+  let mensaje;
+
+  //If se puede poner fuera de un return, no dentro. Para un condicional dentro de un return debemos usar un ternario
+  if (edad >= 18) {
+    mensaje = "Eres mayor de edad";
+  } else {
+    mensaje = "No eres mayor de edad";
+  }
+
+  return (
+    <>
+      <h1>Tienda Virtual </h1>
+      <h3>{mensaje}</h3>
+    </>
+  );
 }
 
 export default Header;
